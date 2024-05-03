cf80d3cb634b6b0e5bc6caec6098ca38cdbe7299
uebeats <uebeats@gmail.com>
2024-05-02 22:09:01 -0400

    Agregue migraciones para la entidad Contacto

---
+++ database/migrations/2024_05_03_020345_create_contactos_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contactos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('email');
+            $table->text('mensaje');
+            // logica de negocio para validar si esta leido o no
+            $table->boolean('status')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contactos');
+    }
+};
